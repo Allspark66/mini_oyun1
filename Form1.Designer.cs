6a1a4497cb757997dfb5a54fb41c059ba6b9678c
mcnunn <mmemmedov19@std.beu.edu.az>
2025-11-03 08:27:54 +0400

    mini_oyun1

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace minioyun
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            panel5 = new Panel();
+            panel4 = new Panel();
+            panel3 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(64, 64, 64);
+            panel1.Controls.Add(panel5);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(0, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(433, 565);
+            panel1.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.White;
+            panel5.Location = new Point(198, 19);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(17, 89);
+            panel5.TabIndex = 3;
+            panel5.Tag = "road";
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.White;
+            panel4.Location = new Point(198, 145);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(17, 89);
+            panel4.TabIndex = 2;
+            panel4.Tag = "road";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.White;
+            panel3.Location = new Point(198, 285);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(17, 89);
+            panel3.TabIndex = 1;
+            panel3.Tag = "road";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(243, 359);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(109, 155);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Location = new Point(198, 425);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(17, 89);
+            panel2.TabIndex = 0;
+            panel2.Tag = "road";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Lime;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button1.Location = new Point(551, 87);
+            button1.Name = "button1";
+            button1.Size = new Size(158, 82);
+            button1.TabIndex = 1;
+            button1.Text = "GAS";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button2.Location = new Point(551, 222);
+            button2.Name = "button2";
+            button2.Size = new Size(158, 82);
+            button2.TabIndex = 2;
+            button2.Text = "BREAK";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(629, 377);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(125, 62);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(498, 377);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(125, 62);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(563, 445);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(125, 62);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 561);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+        private Panel panel5;
+        private Panel panel4;
+        private Panel panel3;
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace minioyun
+{
+    public partial class Form1 : Form
+    {
+        int carSpeed = 0;
+        PictureBox car;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+
+            car = pictureBox1;
+            car.BackColor = Color.Transparent;
+            
+            car.BringToFront();
+
+            timer1.Interval = 50;
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (Control x in panel1.Controls)
+            {
+                if (x.Tag != null && x.Tag.ToString() == "road")
+                {
+                    x.Top += carSpeed;
+                    if (x.Top >= panel1.Height)
+                        x.Top = -x.Height;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            carSpeed = 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            carSpeed = 0;
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (car.Left > 0)
+                car.Left -= 10;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (car.Right < this.ClientSize.Width)
+                car.Left += 10;
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (car.Top < this.ClientSize.Height - car.Height)
+                car.Top += 10;
+        }
+      
+
+
+    }
+}
